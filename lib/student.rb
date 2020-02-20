082dfaed6e3c29669783daaa771c46967a637b6e
David Van Fleet <dvfleet413@gmail.com>
2020-02-20 18:51:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,5 @@
 require_relative "../config/environment.rb"
+require 'pry'
 
 class Student
   attr_accessor :name, :grade
@@ -47,7 +48,7 @@ class Student
   end 
   
   def self.new_from_db(row)
-    
+    binding.pry
   end
   
   def self.find_by_name(name)
